a69a3ab418518a315615e9f31312967f21b309bb
Leo Liu <yliu@almondbank.com>
2017-08-23 14:54:54 -0400

    Initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+target 'SwineReader' do
+	pod 'Swinject', '~> 2.1.0'
+end
+
+# Uncomment if you use SwinjectStoryboard
+# pod 'SwinjectStoryboard', '~> 1.0.0'
---
+++ SwineReader/ViewController.swift
@@ -0,0 +1,210 @@
+import UIKit
+import Swinject
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setup()
+		setupDI()
+	}
+	func setup() {
+		let user = MockUser()
+		user.username = "test001"
+
+		let avatar = MockAvatar(author: user)
+		avatar.imageURL = "http://www.superarts.org/swine/default.png"
+		avatar.create()
+
+		user.avatar = avatar
+		user.create()
+
+		print("====")
+		print("User: \(user)")
+		print("is valid: \(user.isValid())")
+		print("username: \(user.username)")
+		print("avatar: \(user.avatar)")
+		print("----")
+
+		print("Avatar: \(avatar)")
+		print("is valid: \(avatar.isValid())")
+		print("image URL: \(avatar.imageURL)")
+		print("author: \(avatar.author)")
+		print("----")
+	}
+	func setupDI() {
+		let container = DependencyContainer.setup()
+
+		var user = container.resolve(User.self)!
+		user.username = "test001"
+
+		var avatar = container.resolve(Avatar.self, argument: user)!
+		avatar.imageURL = "http://www.superarts.org/swine/default.png"
+		avatar.create()
+
+		user.avatar = avatar
+		user.create()
+
+		print("User: \(user), \(user.isValid())")
+		print("Avatar: \(avatar), \(avatar.isValid())")
+
+		/*
+		let userRepo = container.resolve(UserRepository.self)!
+		userRepo.getUser(uid: 42) { user, error in
+			if let user = user {
+				print("User: \(user)")
+			}
+			if let error = error {
+				print("Error: \(error)")
+			}
+		}
+		*/
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+}
+
+/*
+protocol User {
+	var uid: Int { get set }
+	var username: String { get set }
+}
+
+class EmailUser: User {
+	var uid = -1
+	var username = ""
+	var email = ""
+}
+
+extension EmailUser: CustomStringConvertible {
+    var description: String {
+        return "EmailUser: (\(uid), \(username))"
+    }
+}
+
+typealias UserClosure = ((User?, Error?) -> Void)
+
+protocol UserRepository {
+	//var user: User { get set }
+	func getUser(uid: Int, closure: UserClosure)
+}
+
+class MockSuccessUserRepository: UserRepository {
+	//var user: User
+	func getUser(uid: Int, closure: UserClosure) {
+		let container = DependencyContainer.container
+		var user = container.resolve(User.self)!
+		user.uid = uid
+		closure(user, nil)
+	}
+}
+*/
+
+protocol Indexable {
+	var uid: Int { get set }
+	func isValid() -> Bool
+}
+
+extension Indexable {
+	func isValid() -> Bool {
+		return uid >= 0
+	}
+}
+
+protocol Presentable: Indexable {
+	func create()
+	func show(uid: Int)
+}
+
+protocol Avatar: Presentable {
+	var imageURL: String! { get set }
+	var author: User! { get set }
+	
+	init()
+	init(author: User)
+}
+
+class MockAvatar: Avatar {
+	var uid = -1
+	var imageURL: String!
+	var author: User!
+
+	required init() {
+	}
+	required init(author: User) {
+		self.author = author
+	}
+
+	func create() {
+		uid = 1
+	}
+	func show(uid: Int) {
+		self.uid = uid
+		imageURL = String(format: "http://test.com/image%03d.png", uid)
+	}
+}
+
+protocol User: Presentable {
+	var username: String! { get set }
+	var avatar: Avatar! { get set }
+}
+
+class MockUser: User {
+	var uid = -1
+	var username: String!
+	var avatar: Avatar!
+
+	func create() {
+		uid = 1
+	}
+	func show(uid: Int) {
+		self.uid = uid
+		username = String(format: "test%03d", uid)
+	}
+}
+
+extension MockUser: CustomStringConvertible {
+    var description: String {
+        return "MockUser: (\(uid), \(username))"
+    }
+}
+
+protocol Post: Presentable {
+	var title: String! { get set }
+}
+
+/*
+protocol Post {
+	var uid: Int { get set }
+	var title: String { get set }
+	init 
+	func create(title: String) -> Post
+	func show(uid: Int) -> Post
+}
+
+class MockPost: Post {
+	var uid = -1
+	var title = ""
+	init(title: String) {
+		self.title = title
+		uid = 1
+	}
+	func create(title: String) -> Post
+	func show(uid: Int) -> Post {
+		return self
+	}
+}
+*/
+
+class DependencyContainer {
+	static let container = Container()
+	static func setup() -> Container {
+		//container.register(UserRepository.self) { _ in MockSuccessUserRepository() }
+		//container.register(User.self) { _ in EmailUser() }
+		container.register(User.self) { _ in MockUser() }
+		container.register(Avatar.self) { _, user in MockAvatar(author: user) }
+		return container
+	}
+}
